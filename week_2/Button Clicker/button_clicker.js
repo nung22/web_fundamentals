dffffd407a6f0df69feefc8c781b6784807088f2
Nicholas Ung <ung.nicholask@gmail.com>
2022-10-14 22:24:18 -0700

    finished button clicker assignment

---
+++ week_2/Button Clicker/button_clicker.js
@@ -0,0 +1,9 @@
+// Change login button to say logout when clicked
+function logout(element){
+    element.innerText = "Logout";
+}
+
+// Remove the Add Definition button when it is clicked
+function removeAddDef(element){
+    element.remove();
+}
