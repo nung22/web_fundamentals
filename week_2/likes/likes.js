ff8d0ea283ddf86d9bac94181bed63a97b839243
Nicholas Ung <ung.nicholask@gmail.com>
2022-10-17 12:03:04 -0700

    finished likes assignment

--- week_2/likes/likes.js
+++ week_2/likes/likes.js
@@ -1,5 +1,17 @@
+// ARRAY NUMLIKES STORES NUMBER OF LIKES FOR EACH POST
+var numLikes = [document.querySelector("#num-likes-top"),document.querySelector("#num-likes-middle"),document.querySelector("#num-likes-bottom")];
 
-function addLike(){
-    var numLikes = document.querySelector("#num-likes");
-    numLikes.innerText++;
+// ADDS A LIKE TO THE TOP POST
+function addLikeTop(){
+    numLikes[0].innerText++;
+}
+
+// ADDS A LIKE TO THE MIDDLE POST
+function addLikeMiddle(){
+    numLikes[1].innerText++;
+}
+
+// ADDS A LIKE TO THE BOTTOMPOST
+function addLikeBottom(){
+    numLikes[2].innerText++;
 }
