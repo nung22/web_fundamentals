17fdb0c96966efa630120f28dbddfef3eabcf626
Nicholas Ung <ung.nicholask@gmail.com>
2022-10-19 12:00:27 -0700

    finished yellow belt exam

--- yellow-belt-exam/yellow_belt.js
+++ yellow-belt-exam/yellow_belt.js
@@ -1,1 +1,18 @@
-console.log("Exam is loading...");
+// Sends alert to user that their cart is empty
+function cartStatus(){
+    alert('Your Cart is empty!');
+}
+
+// Changes original image to a new image
+function changePic(element){
+    element.src = ("./assets/succulents-2.jpg");
+}
+// Reverts changed image back to original image
+function restorePic(element){
+    element.src = ("./assets/succulents-1.jpg");
+}
+
+// Removes cookie message
+function removeMessage(){
+    document.querySelector(".cookie").remove();
+}
